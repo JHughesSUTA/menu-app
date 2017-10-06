1180689ac028098967abd8b0f9426765da4ec71e
James Hughes <james.a.hughes@gmail.com>
2017-10-05 22:27:56 -0500

    Stylize more of admin page

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,10 @@ class Item < ApplicationRecord
   has_many :orders
   has_many :carted_items
 
-  validates :name, :description, :price, :category_id, :lunch, :dinner, presence: true
+  validates :name, :description, :price, :category_id, presence: true
   validates :price, :category_id, numericality: { greater_than: 0 }
+
+  # ensures that at least one menu is chosen.  TODO: change validation error mesage
+  validates :lunch, presence: { if: -> { dinner.blank? } }
+  validates :dinner, presence: { if: -> { lunch.blank? } }
 end
